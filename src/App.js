c8580f0694572b7aa7a245a02639116a8e789b5e
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-10-20 12:48:06 +0200

    added header component
    added Header component and styling for the component.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import Header from './components/header';
+
+function App() {
+  return(
+    <div className="app">
+      <Header />
+      <h1>Welcome to the garden design website</h1>
+  </div>
+  );
+}
+
+export default App;
