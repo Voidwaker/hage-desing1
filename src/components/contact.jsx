61ddae3b8938afbe2b3482fdc96767e09e3e6006
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-10-21 02:40:26 +0200

    Update contact.jsx
    updated contact form to work in react (forgot to import useState and some other minor things

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,19 +1,49 @@
 import react from 'react';
+import './contact.css';
+import  {useState} from 'react';
 
+const Contact = ()=>{
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
 
-const Contact = () => {
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(name, email, message);
+    };
+
     return (
         <section>
             <h2>Contact Us</h2>
-            <form>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" />
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message"></textarea>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <Input
+
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                <label htmlFor="email">Email</label>
+                <Input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <label htmlFor="message">Message</label>
+                <textarea
+                id="message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
                 <button type="submit">Send</button>
             </form>
         </section>
     );
 };
+
+export default Contact;
