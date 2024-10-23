572d99b33dc35f19df646d3d107c319de91b6ec6
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-10-23 10:23:54 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return(
         <footer className="footer">
         <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Berits Hage Design. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Berits Hage Design.</p>
         <div className='footer-links'>
         <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>Facebook</a>
         <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>Instagram</a>
